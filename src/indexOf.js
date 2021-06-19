c88b865f497ac09a0d793aafa7759ab7e8d7ce9d
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-06-19 09:37:38 -0500

    submitted solution

--- src/indexOf.js
+++ src/indexOf.js
@@ -1,4 +1,13 @@
+//TODO  implement a linear search algorithm that passes the tests.
+
 function indexOf(isMatch, elements) {
+  if (Array.isArray(elements)) {
+    for (let index = 0, length = elements.length; index < length; index++) {
+      if (isMatch(elements[index], index, elements)) {
+        return index;
+      }
+    }
+  }
   return -1;
 }
 
